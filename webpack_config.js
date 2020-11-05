9d356530f92d3a6e8c7bcc080a74a5cbe7246b49
osipov-andrey <osyapov@bk.ru>
2020-11-05 22:35:58 +0400

    Added eslint and etc.

--- webpack_config.js
+++ webpack_config.js
@@ -1,63 +1,103 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  ]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
+
+const fileName = (ext) => isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`
+
+console.log('IS PROD', isProd)
+console.log('IS DEV', isDev)
+
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    mode: "development",
-    entry: "./index.js",
-    output: {
-        filename: "bundle.[hash].js",
-        path: path.resolve(__dirname, 'dist')
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
+  output: {
+    filename: fileName('js'),
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
     },
-    resolve: {
-        extensions: ['.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src/core')
-        }
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HTMLWebpackPlugin({
-            template: "index.html"
-        }),
-        new CopyPlugin(
+  },
+  devtool: isDev ? 'inline-source-map' : false,
+  devServer: {
+    contentBase:
+        isDev ? path.join(__dirname, 'src') : path.join(__dirname, 'dist'),
+    port: 8080,
+    hot: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: 'index.html',
+      minify: {
+        removeComments: isProd,
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CopyPlugin(
+        {
+          patterns: [
             {
-                patterns: [
-                    {
-                        from: path.resolve(__dirname, 'src/favicon.ico'),
-                        to: path.resolve(__dirname, 'dist'),
-                    }
-                ]
-            }
-        ),
-        new MiniCssExtractPlugin({
-            filename: 'bundle.[hash].css'
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
+              from: path.resolve(__dirname, 'src/favicon.ico'),
+              to: path.resolve(__dirname, 'dist'),
             },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                    }
-            }
-        ]
-    }
+          ],
+        }
+    ),
+    new MiniCssExtractPlugin({
+      filename: fileName('css'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true,
+            },
+          },
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: jsLoaders(),
+
+      },
+    ],
+  },
 }
