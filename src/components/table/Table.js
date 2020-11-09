692188a2edd41e15231dce0a876e94c5702e1f1a
Осипов Андрей Алексеевич <AOsipov@vertex.spb.ru>
2020-11-09 11:33:01 +0300

    Creating table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,34 +1,10 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {createTable} from '@/components/table/table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
   toHTML() {
-    return `
-            <div class="row">
-
-                <div class="row-info"></div>
-
-                <div class="row-data">
-
-                    <div class="column">a</div>
-                    <div class="column">b</div>
-                    <div class="column">c</div>
-
-                </div>
-
-            </div>
-             <div class="row">
-
-                <div class="row-info">1</div>
-
-                <div class="row-data">
-                    <div class="cell selected" contenteditable="true">11</div>
-                    <div class="cell" contenteditable="true">22</div>
-                    <div class="cell" contenteditable="true">33</div>
-                </div>
-
-            </div>
-    `
+    return createTable()
   }
 }
