c70722520526288d619b9c87073ebfa818f09db7
LiliaVasileva <lilia.vasileva@uniberg.com>
2024-03-13 14:39:09 +0200

    Fix bug enable/disable style for cell

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -36,9 +36,8 @@ export class Toolbar extends ExcelStateComponent {
     if ($target.data.type === 'button') {
       const value = JSON.parse($target.data.value)
       this.$emit('toolbar:applyStyle', value)
-      // debugger
-      // this.setState(value)
-      // this.storeChanged(value)
+      this.setState(value)
+      this.storeChanged(value)
     }
   }
 }
